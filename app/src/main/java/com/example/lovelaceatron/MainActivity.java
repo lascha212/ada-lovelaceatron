77f8a2d81c6e3dccc177234a1780def99577c880
Laura <laura.schauer212@gmail.com>
2021-04-18 13:28:51 +0100

    side navigation

--- app/src/main/java/com/example/lovelaceatron/MainActivity.java
+++ app/src/main/java/com/example/lovelaceatron/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lovelaceatron;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
@@ -16,6 +19,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.navigation.NavigationView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,71 +27,31 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    // define variables
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Button buttonRegister;
-    private ProgressDialog progressDialog;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar((androidx.appcompat.widget.Toolbar) toolbar);
 
-        buttonRegister = (Button) findViewById(R.id.buttonRegister);
-        progressDialog = new ProgressDialog(this);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
     }
 
-    private void registerUser() {
-        final String email = editTextEmail.getText().toString();
-        final String password = editTextPassword.getText().toString();
 
-        progressDialog.setMessage("Registering user.");
-        progressDialog.show();
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.URL_REGISTER,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        progressDialog.dismiss();
 
-                        try {
-                            JSONObject json = new JSONObject(response);
-                            // gets the value at the key "message"
-                            Toast.makeText(getApplicationContext(), json.getString("message"), Toast.LENGTH_SHORT).show();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        progressDialog.hide();
-                        // display the error
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("email", email);
-                params.put("password", password);
-                return params;
-            }
-        };
-
-        // add the request to the request queue
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
-    }
-
-    @Override
-    public void onClick(View v) {
-        registerUser();
-    }
 }
