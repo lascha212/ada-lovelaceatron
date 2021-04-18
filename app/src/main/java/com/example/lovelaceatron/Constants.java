7e3dd14197211e6a79a78b446daa411370849ed2
Laura <laura.schauer212@gmail.com>
2021-04-18 11:22:52 +0100

    tried to access database

---
+++ app/src/main/java/com/example/lovelaceatron/Constants.java
@@ -0,0 +1,6 @@
+package com.example.lovelaceatron;
+
+public class Constants {
+    public static final String ROOT_URL = "http://192.168.0.29/lovelaceatron/v1/registerUser.php"; // IP: 192.168.0.29
+    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+}
--- app/src/main/java/com/example/lovelaceatron/MainActivity.java
+++ app/src/main/java/com/example/lovelaceatron/MainActivity.java
@@ -2,13 +2,92 @@ package com.example.lovelaceatron;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // define variables
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        progressDialog = new ProgressDialog(this);
+    }
+
+    private void registerUser() {
+        final String email = editTextEmail.getText().toString();
+        final String password = editTextPassword.getText().toString();
+
+        progressDialog.setMessage("Registering user.");
+        progressDialog.show();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject json = new JSONObject(response);
+                            // gets the value at the key "message"
+                            Toast.makeText(getApplicationContext(), json.getString("message"), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        // display the error
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        // add the request to the request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View v) {
+        registerUser();
     }
 }
